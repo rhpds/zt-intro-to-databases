f080f3787a17263904f8d25ebb063b625694c245
baraka <126705717+Brillar0101@users.noreply.github.com>
2026-06-22 10:23:23 -0400

    Unregister and re-register the system

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 #install adminer
+# Unregister and register the VM
+subscription-manager clean
+subscription-manager register --activationkey=12-5-22-instruqt --org=12451665 --force
+
 mkdir /var/www/html/adminer
 wget -O /var/www/html/adminer/index.php https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-en.php
 
